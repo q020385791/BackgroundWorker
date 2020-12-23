9a7d8866ebd2a5ce3ff7b6d14232c64a4b81b872
sai wanza <q020385791@gmail.com>
2020-12-23 21:11:07 +0800

    加入專案檔案。

---
+++ BackgroundWorker/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace BackgroundWorker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.bkWorker = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(61, 106);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "開始";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(264, 106);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 1;
+            this.btn_stop.Text = "停止";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(61, 40);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(278, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 191);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.ComponentModel.BackgroundWorker bkWorker;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ BackgroundWorker/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using bw=System.ComponentModel.BackgroundWorker;
+
+
+namespace BackgroundWorker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            initProgressBar();
+            initbackgroundWorker();
+        }
+        private bw bw;
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            //如果不是正在執行才做
+            if (!bkWorker.IsBusy)
+            {
+                //避免多次點到
+                this.btn_start.Text = "執行中";
+                this.btn_start.Enabled = false;
+                //進度條重置
+                progressBar1.Value =0;
+                bw.RunWorkerAsync();
+            }
+        }
+
+
+        #region 初始化
+        /// <summary>
+        /// 初始化進度條
+        /// </summary>
+        public void initProgressBar()
+        {
+            progressBar1.Step = 1;
+        }
+        /// <summary>
+        /// 初始化背景作業
+        /// </summary>
+        public void initbackgroundWorker()
+        {
+            bw = new bw();
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+
+            //下面掛上背景作業要做的事情
+            //包括要執行的作業、完成後要做的事情、當執行修改的時候
+            
+            
+            //要執行的作業
+            bw.DoWork += new DoWorkEventHandler(bw_Dowork);
+
+            //當作業完成後要做的事情
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_WorkerCompleted);
+
+            bw.ProgressChanged += new ProgressChangedEventHandler(bw_Change);
+        }
+        #endregion
+
+        /// <summary>
+        /// 背景執行作業
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void bw_Dowork(object sender, DoWorkEventArgs e)
+        {
+            
+
+            for (int i = 1; i <= 10; i++)
+            {
+                //先判斷是否有點取消
+                if (bw.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(700);
+                    //觸發改變事件
+                    bw.ReportProgress(i * 10);
+                }
+              
+            }
+
+        }
+
+        /// <summary>
+        /// 背景作業完成之後要做的事情
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void bw_WorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //判斷是不是被點取消
+
+            if (e.Cancelled == true)
+            {
+                btn_start.Text = "已取消";
+                //錯誤判斷
+            } else if (e.Error!=null)
+            {
+
+                btn_start.Text = "錯誤："+e.Error.Message;
+            }
+            else
+            {
+                btn_start.Text = "完成作業";
+            }
+            
+        }
+
+        /// <summary>
+        /// 觸發改變動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void bw_Change(object sender,ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            //如果有設定可以中途終止的話
+            if (bw.WorkerSupportsCancellation==true)
+            {
+                //取消該作業
+                bw.CancelAsync();
+                //避免多次點到
+                this.btn_start.Text = "已取消";
+            }
+        }
+    }
+}
